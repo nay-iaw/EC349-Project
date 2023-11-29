097e9a4ff475311087f040a57cf062673b6bcb84
Wai Yan <waiyanfong1020123@gmail.com>
2023-11-29 21:54:14 +0000

    Multinomial Regression Testing

--- Assignment Project.R
+++ Assignment Project.R
@@ -37,3 +37,27 @@ reviewx_train <-review_train[,-1]
 reviewy_train <-review_train[,1]
 reviewx_test <-review_test[,-1]
 reviewy_test <-review_test[,1]
+
+#Preliminary Multinomial Logistic Regression and Prediction
+install.packages("nnet")
+library(nnet)
+review_multinom_model <- multinom(stars ~ useful + funny + cool, data = review_train)
+summary (review_multinom_model)
+multinom_prediction <- predict(review_multinom_model, newdata = review_test)
+summary (multinom_prediction)
+review_test$stars <- factor(review_test$stars)
+summary (review_test$stars)
+
+#Merge datasets- train & test
+merge_review_user <- merge (review_data_small, user_data_small, by ="user_id")
+test_obs2 <- sample(1:nrow(merge_review_user), 10000)
+review_test2 <- merge_review_user[test_obs2, ]
+review_train2 <- merge_review_user[-test_obs2, ]
+
+#Preliminary Multinomial Logistic Regression and Prediction adding one more variable
+review_multinom_model2 <- multinom(stars ~ useful.x + funny.x + cool.x + average_stars, data = review_train2)
+summary(review_multinom_model2)
+multinom_prediction2 <- predict(review_multinom_model2, newdata = review_test2)
+summary (multinom_prediction2)
+review_test2$stars <- factor(review_test2$stars)
+summary (review_test2$stars)
