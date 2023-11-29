3449effa4ff265da90cc7aea91d06554b85642e5
Wai Yan <waiyanfong1020123@gmail.com>
2023-11-29 16:36:26 +0000

    Initial commit

---
+++ Assignment Project.R
@@ -0,0 +1,18 @@
+#Clear
+cat("\014")  
+rm(list=ls())
+
+#Set Directory as appropriate
+setwd("/Users/waiyan_1020/Desktop/EC349 Project")
+
+#Pre-Processing Yelp Academic Data for the Assignment
+library(jsonlite)
+
+#Load Different Data
+business_data <- stream_in(file("yelp_academic_dataset_business.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+checkin_data  <- stream_in(file("yelp_academic_dataset_checkin.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+tip_data  <- stream_in(file("yelp_academic_dataset_tip.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+
+#Load smaller user and review data
+load("/Users/waiyan_1020/Desktop/EC349 Project/yelp_review_small.Rda")
+load("/Users/waiyan_1020/Desktop/EC349 Project/yelp_user_small.Rda")
