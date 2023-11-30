5c1ebe6fb612c48ead27de9559cd40210af8b0fd
Wai Yan <waiyanfong1020123@gmail.com>
2023-11-30 19:18:14 +0000

    Random Forest 1

--- Assignment Project.R
+++ Assignment Project.R
@@ -67,4 +67,35 @@ business_checkin_merged <- left_join(business_data, checkin_data2, by = "busines
 business_checkin_merged2 <- business_checkin_merged %>%
   select(business_id, stars, review_count, checkin_count)
 
+#Merge business and user data
+user_business_merged <- left_join(user_review_merged, business_checkin_merged2, by = "business_id")
+user_business_merged2 = subset(user_business_merged, select = -c(1,2,3,8,9))
 
+#Change variables from int to numeric; stars to factor
+str(user_business_merged2)
+user_business_merged2$useful <- as.numeric(user_business_merged2$useful)
+user_business_merged2$funny <- as.numeric(user_business_merged2$funny)
+user_business_merged2$cool<- as.numeric(user_business_merged2$cool)
+user_business_merged2$word_count <- as.numeric(user_business_merged2$word_count)
+user_business_merged2$review_count.x <- as.numeric(user_business_merged2$review_count.x)
+user_business_merged2$review_count.y <- as.numeric(user_business_merged2$review_count.y)
+user_business_merged2$stars.x <- as.factor(user_business_merged2$stars.x)
+
+#Split the data into test and training
+set.seed(1)
+test_obs <- sample(1:nrow(user_business_merged2), 10000)
+review_test <- user_business_merged2[test_obs, ]
+review_train <- user_business_merged2[-test_obs, ]
+
+
+#Handle missing values- replace NA with means/median
+review_train$review_count.x <- ifelse(is.na(review_train$review_count.x), median(review_train$review_count.x, na.rm = TRUE), review_train$review_count.x)
+review_train$average_stars<- ifelse(is.na(review_train$average_stars), mean(review_train$average_stars, na.rm = TRUE), review_train$average_stars)
+review_train$sum_compliments <- ifelse(is.na(review_train$sum_compliments), median(review_train$sum_compliments, na.rm = TRUE), review_train$sum_compliments)
+review_train$checkin_count <- ifelse(is.na(review_train$checkin_count), median(review_train$checkin_count, na.rm = TRUE), review_train$checkin_count)
+sum(is.na(review_train))
+
+#Random Forest
+install.packages("randomForest")
+library(randomForest)
+model_RF <- randomForest(stars.x ~ ., data = review_train)
